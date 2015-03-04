25e2d3d2ad0ff3f2543d7fde4aac02aa66a87e2f
Jan Rabe <janrabe@kibotu.net>
2015-03-05 00:01:07 +0100

    added clojure

---
+++ Assets/Scripts/Clojure/ClojureTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClojureTest : MonoBehaviour {
+
+    static Func<int> GetNewUidClojure()
+    {
+        int uid = 0;
+        return () => uid++;
+    }
+
+    public Func<int> GetNewUid = GetNewUidClojure();
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Uid: " + GetNewUid());
+        }
+    }
+}
