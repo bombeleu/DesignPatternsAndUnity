e09e7fb083bc811a01747e03bc362e932b6693d6
Jan Rabe <janrabe@kibotu.net>
2015-03-05 08:45:36 +0100

    added ienumerator

---
+++ Assets/Scripts/Clojure/ClojureTest2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ClojureTest2 : MonoBehaviour 
+{
+    public static volatile IEnumerator<int> GetNewUid = GetNewUidClojure();
+
+    private static IEnumerator<int> GetNewUidClojure()
+    {
+        var uid = 0;
+        while(true) 
+            yield return uid++;
+    }
+	
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Uid: " + GetNewUid.Current);
+            GetNewUid.MoveNext();
+        }
+	}
+}
